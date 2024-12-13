c56ade1dcd3cee291f57dc220ad764be16c06c75
Super-Nova-Software <supernovasoftwareco@gmail.com>
2024-12-13 14:48:21 +0300

    fix

--- src/components/global/group-side-widget/index.tsx
+++ src/components/global/group-side-widget/index.tsx
@@ -13,21 +13,24 @@ const GroupSideWidget = () => {
         "border-themeGray lg:sticky lg:top-0 mt-10 lg:mt-0 rounded-xl overflow-hidden",
       )}
     >
-     <Image
-      alt="Mountains"
-      src={'https://brave.com/static-assets/images/optimized/blog/photo-scholarship/images/student-ntp-sam-richter.webp'}
+    <Image
+      src="https://brave.com/static-assets/images/optimized/blog/photo-scholarship/images/student-ntp-sam-richter.webp"
+      alt="thumbnail"
+      className="w-full aspect-video"
+      width={1280}
+      height={720}
       placeholder="blur"
-      quality={100}
-      fill
-      sizes="100vw"
+      blurDataURL="blur"
+      quality={50}
+      sizes="100%"
       style={{
         objectFit: 'cover',
       }}
     />
       <div className="flex flex-col p-5 gap-y-2">
-        <h2 className="font-bold text-lg">Test</h2>
+        <h2 className="font-bold text-lg">Campaigns</h2>
         <p className="text-sm text-themeTextGray">
-          group.description && truncateStringgroup.description
+        Campaigns.description
         </p>
       </div>
       <Separator orientation="horizontal" className="bg-themeGray" />
